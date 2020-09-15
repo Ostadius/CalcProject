bccd1d0acd048e7462643f67a6b81bd72a8a808c
elyas <elyas.arkin@hotmail.se>
2020-09-15 13:47:26 +0200

    js file

---
+++ index.js
@@ -0,0 +1,11 @@
+var num1 = document.getElementById("input1");
+
+var num2 = document.getElementById("input2");
+
+var result = document.getElementById("resul");
+
+function add (num1, num2) {
+	    
+	    return result.innerHTML = num1 + num2;
+	    
+}
