2a5e4b34223eb167102cc95d6f6a743d97363285
elyas <elyas.arkin@hotmail.se>
2020-09-15 14:07:07 +0200

    updated

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var num1 = document.getElementById("input1");
 var num2 = document.getElementById("input2");
 
 var result = document.getElementById("resul");
+var calBtn = document.getElementById("sub");
+
+calBtn.addEventListener("click" add)
 
 function add (num1, num2) {
-	    
-	    return result.innerHTML = num1 + num2;
-	    
+		    
+		    return result.innerHTML ="G";
+		    
 }
